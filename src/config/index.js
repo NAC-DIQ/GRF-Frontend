f2da47f0fb650c77716940ceecef56f816250ae5
NAC_DIQ <nachiket@diquery.com>
2024-05-04 18:28:23 +0530

    new api added

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,4 @@
 // export const API_URL = 'https://goldenratiofilms.herokuapp.com/api/';
 // export const API_URL =
 // 'http://ec2-3-108-223-46.ap-south-1.compute.amazonaws.com/api/';
-export const API_URL = 'https://3.111.144.198/';
+export const API_URL = 'https://13.202.32.140/';
